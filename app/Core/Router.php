98ef8d655bd9e1d76a8c7540d59ea71242bc3f62
kiaamaaio <kiaamaaio@gmail.com>
2024-06-17 00:19:52 +0900

    putリクエスト処理を追加

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -4,11 +4,13 @@ namespace HttpRequestReceiver\Core;
 
 use HttpRequestReceiver\Exception\NotFoundException;
 use HttpRequestReceiver\Http\Request\PostRequestProcessor;
+use HttpRequestReceiver\Http\Request\PutRequestProcessor;
 
 class Router
 {
     private const array PROCESSOR_CLASSES = [
-        'POST' => PostRequestProcessor::class
+        'POST' => PostRequestProcessor::class,
+        'PUT' => PutRequestProcessor::class
     ];
 
     public function getProcessorClass(string $request_method): string
